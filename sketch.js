e73c9752fb56f6307a4b3a4d0a9a75c92098048f
sudipta456 <susanta72460@gmail.com>
2020-12-11 14:31:05 +0530

    adding comments

--- sketch.js
+++ sketch.js
@@ -18,18 +18,18 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
-
+//layer1
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+//layer2
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
-
+//layer3
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
